697a11bb86396b9ec62b34ed9d82cda582edd6f3
Daniele Muffato <daniele.muffato@studio.unibo.it>
2023-04-09 21:47:28 +0200

    [FIX] gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,21 @@
+// ".\gradlew.bat" instead of "./gradlew" if you are on windows
+
 plugins { 
-	// support for java
+	 // Apply the java plugin to add support for Java
     java
 
-    // support for running application on the terminal
-	// ".\gradlew.bat" instead of "./gradlew" if you are on windows
+	// Apply the application plugin to add support for building a CLI application
 	// run application via "./gradlew run" 
     application
 
     // add task to create a runnable jar
 	// create runnable jar via "./gradlew shadowJar"
 	// the runnable jar will be found in "build/lib/<name>.jar"
-	id("com.github.johnrengelman.shadow") version "7.0.0"
+	id("com.github.johnrengelman.shadow") version "8.1.1"
 
 	// support for code quality checkers
 	// check code quality via "./gradlew check"
-	id("org.danilopianini.gradle-java-qa") version "0.41.0"
+	id("org.danilopianini.gradle-java-qa") version "1.6.0"
 }
 
 application{
@@ -31,11 +32,15 @@ dependencies {
 
 	// support for Junit test library
 	// test code via "./gradlew test"
-	val jUnitVersion = "5.9.1"
+	val jUnitVersion = "5.9.2"
 	testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")	
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()	
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events(*org.gradle.api.tasks.testing.logging.TestLogEvent.values())
+        showStandardStreams = true
+    }
 }
