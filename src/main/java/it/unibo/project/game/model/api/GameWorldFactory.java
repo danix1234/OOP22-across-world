e1e1161ec1f6bccdd0a5312ff5ad9e1f4a5b4878
Daniele Muffato <daniele.muffato@studio.unibo.it>
2023-04-12 09:49:11 +0200

    [ADD] GameWorldFactory

---
+++ src/main/java/it/unibo/project/game/model/api/GameWorldFactory.java
@@ -0,0 +1,5 @@
+package it.unibo.project.game.model.api;
+
+public interface GameWorldFactory {
+    public GameWorld createGameWorld();
+}
---
+++ src/main/java/it/unibo/project/game/model/impl/GameWorldFactoryImpl.java
@@ -0,0 +1,14 @@
+package it.unibo.project.game.model.impl;
+
+import it.unibo.project.game.model.api.GameWorld;
+import it.unibo.project.game.model.api.GameWorldFactory;
+
+public class GameWorldFactoryImpl implements GameWorldFactory {
+
+    @Override
+    public GameWorld createGameWorld() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createGameWorld'");
+    }
+    
+}
