7f9443596a37082aeb06f3dac8a854d576adee02
Daniele Muffato <daniele.muffato@studio.unibo.it>
2023-04-14 10:45:59 +0200

    [FEAT] implemented some shared input actions

--- src/main/java/it/unibo/project/input/impl/SharedInputHandler.java
+++ src/main/java/it/unibo/project/input/impl/SharedInputHandler.java
@@ -1,34 +1,49 @@
 package it.unibo.project.input.impl;
 
+import it.unibo.project.controller.core.api.SceneType;
+import it.unibo.project.controller.core.impl.LauncherImpl;
 import it.unibo.project.input.api.Action;
 import it.unibo.project.input.api.InputHandler;
 
 //class to use in shared action type, like change of scene, or exit button
 public class SharedInputHandler implements InputHandler {
 
-
     @Override
     public void executeAction(Action action) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'executeAction'");
+        switch (action) {
+            case EXIT_APP:
+                System.exit(0);
+                break;
+            case CHANGE_SCENE_TO_MENU:
+                LauncherImpl.LAUNCHER.setScene(SceneType.MENU);
+                break;
+            case CHANGE_SCENE_TO_SHOP:
+                LauncherImpl.LAUNCHER.setScene(SceneType.SHOP);
+                break;
+            case CHANGE_SCENE_TO_GAME:
+                LauncherImpl.LAUNCHER.setScene(SceneType.GAME);
+                break;
+            case CHANGE_SCENE_TO_OVER:
+                LauncherImpl.LAUNCHER.setScene(SceneType.OVER);
+                break;
+            default:
+                throw new UnsupportedOperationException("wrong action type passed");
+        }
     }
 
     @Override
     public void storeAction(Action action) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'storeAction'");
+        throw new UnsupportedOperationException("not needed method");
     }
 
     @Override
     public void executeStoredAction() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'executeStoredAction'");
+        throw new UnsupportedOperationException("not needed method");
     }
 
     @Override
     public void clearStoredAction() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'clearStoredAction'");
+        throw new UnsupportedOperationException("not needed method");
     }
-    
+
 }
