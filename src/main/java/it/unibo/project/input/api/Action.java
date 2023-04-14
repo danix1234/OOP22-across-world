2d959b8d79e42b9600ccdb46514575eec976d1f3
Daniele Muffato <daniele.muffato@studio.unibo.it>
2023-04-14 10:24:33 +0200

    [ADD] some basic action types

--- src/main/java/it/unibo/project/input/api/Action.java
+++ src/main/java/it/unibo/project/input/api/Action.java
@@ -1,4 +1,9 @@
 package it.unibo.project.input.api;
 
 public enum Action {
+    CHANGE_SCENE_TO_MENU,
+    CHANGE_SCENE_TO_SHOP,
+    CHANGE_SCENE_TO_GAME,
+    CHANGE_SCENE_TO_OVER,
+    EXIT_APP,
 }
