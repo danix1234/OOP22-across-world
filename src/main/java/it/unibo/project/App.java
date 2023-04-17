3c3fc160f3c1c7f01c05423ba13e9f241b6d3112
Daniele Muffato <daniele.muffato@studio.unibo.it>
2023-04-17 12:54:22 +0200

    [JAVADOC] javadoc for App class

--- src/main/java/it/unibo/project/App.java
+++ src/main/java/it/unibo/project/App.java
@@ -2,10 +2,18 @@ package it.unibo.project;
 
 import it.unibo.project.controller.core.impl.LauncherImpl;
 
+/**
+ * Class {@code App}, contains the {@code main} function.
+ */
 public final class App {
     private App() {
     }
 
+    /**
+     * entry point of the program.
+     * 
+     * @param args strings passed from terminal
+     */
     public static void main(final String[] args) {
         LauncherImpl.LAUNCHER.start();
     }
