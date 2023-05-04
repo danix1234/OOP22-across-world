37f6f22d55026aa51e9e9b204f3d5f23118c5960
Alessandro Torelli <toryst4rk@gmail.com>
2023-05-04 15:26:19 +0200

    aggiunto loadMap

--- src/main/java/it/unibo/project/input/impl/SharedInputHandler.java
+++ src/main/java/it/unibo/project/input/impl/SharedInputHandler.java
@@ -27,6 +27,7 @@ public class SharedInputHandler implements InputHandler {
                 LauncherImpl.LAUNCHER.setScene(SceneType.SHOP);
                 break;
             case CHANGE_SCENE_TO_GAME:
+                LauncherImpl.LAUNCHER.loadMap();
                 LauncherImpl.LAUNCHER.setScene(SceneType.GAME);
                 break;
             case CHANGE_SCENE_TO_OVER:
