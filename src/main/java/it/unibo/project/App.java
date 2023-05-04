20e78f49496cc8a9549035d7e1bf1fedb965b84a
Daniele Muffato <daniele.muffato@studio.unibo.it>
2023-05-04 17:16:04 +0200

    [FIX] removed brute scene change in App

--- src/main/java/it/unibo/project/App.java
+++ src/main/java/it/unibo/project/App.java
@@ -1,8 +1,6 @@
 package it.unibo.project;
 
 import it.unibo.project.controller.core.impl.LauncherImpl;
-import it.unibo.project.input.api.Action;
-import it.unibo.project.input.impl.SharedInputHandler;
 
 /**
  * Class {@code App}, contains the {@code main} function.
@@ -19,6 +17,5 @@ public final class App {
     public static void main(final String[] args) {
         LauncherImpl.LAUNCHER.start();
         LauncherImpl.LAUNCHER.showWindow();
-        new SharedInputHandler().executeAction(Action.CHANGE_SCENE_TO_GAME);
     }
 }
