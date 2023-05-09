c4ba795a2157393d3f8c8f2656473ea247bdcc2c
AleToro7 <toryst4rk@gmail.com>
2023-05-09 17:44:31 +0200

    controlli collisione in movementLogic

--- src/main/java/it/unibo/project/game/logic/impl/MovementLogicImpl.java
+++ src/main/java/it/unibo/project/game/logic/impl/MovementLogicImpl.java
@@ -1,15 +1,27 @@
 package it.unibo.project.game.logic.impl;
 
+import it.unibo.project.controller.core.api.SceneType;
+import it.unibo.project.controller.core.impl.LauncherImpl;
 import it.unibo.project.game.logic.api.MovementLogic;
+import it.unibo.project.utility.Vector2D;
 
 public class MovementLogicImpl implements MovementLogic {
 
     @Override
     public void movePlayer(final int x, final int y) {
-        // TODO check collision
-        
-        // do this only after it is sure player won't collide with obstacles
-        // LauncherImpl.LAUNCHER.getPlayer().move(x,y);  
+        CheckCollisionImpl checker = new CheckCollisionImpl();
+
+        if (checker.checkDynamicObstacleCollision().isEmpty()
+                && checker.checkStaticObstacleCollision(new Vector2D(x, y)).isEmpty()
+                && !checker.checkWallCollision()
+                && !checker.checkFinishLineCollision()) {
+
+            LauncherImpl.LAUNCHER.getPlayer().move(x, y);
+        } else if (checker.checkFinishLineCollision()) {
+            LauncherImpl.LAUNCHER.setScene(SceneType.MENU);
+        } else if (checker.checkDynamicObstacleCollision().isPresent()) {
+            LauncherImpl.LAUNCHER.setScene(SceneType.OVER);
+        }
     }
 
     @Override
