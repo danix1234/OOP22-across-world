bc9ef1320538117d648115e08042c7990f15c660
Motta <roberto.dellamotta2@studio.unibo.it>
2023-05-09 15:59:20 +0200

    move player

--- src/main/java/it/unibo/project/game/logic/impl/MovementLogicImpl.java
+++ src/main/java/it/unibo/project/game/logic/impl/MovementLogicImpl.java
@@ -1,13 +1,13 @@
 package it.unibo.project.game.logic.impl;
 
+import it.unibo.project.controller.core.impl.LauncherImpl;
 import it.unibo.project.game.logic.api.MovementLogic;
 
 public class MovementLogicImpl implements MovementLogic {
 
     @Override
     public void movePlayer(final int x, final int y) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'movePlayer'");
+        LauncherImpl.LAUNCHER.getPlayer().move(x,y);  
     }
 
     @Override
