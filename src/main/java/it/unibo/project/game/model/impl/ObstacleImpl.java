6c2ea1d4a735cb3d3e8b9b1008ea8a043bacb120
Daniele Muffato <daniele.muffato@studio.unibo.it>
2023-05-11 16:16:26 +0200

    [FIX] trunk not drawn

--- src/main/java/it/unibo/project/game/model/impl/ObstacleImpl.java
+++ src/main/java/it/unibo/project/game/model/impl/ObstacleImpl.java
@@ -7,13 +7,13 @@ import java.util.Objects;
 
 public class ObstacleImpl extends EntityImpl implements Obstacle {
 	private final ObstacleType type;
-	private Vector2D pixelPos = new Vector2D(0, 0);
+	private Vector2D pixelPos = new Vector2D(50, 0);
 	private final int speed;
 	private final int moveLeft;
 
 	public ObstacleImpl(final Vector2D initialPos, final ObstacleType type) {
 		super(initialPos, type == ObstacleType.CAR_DX || type == ObstacleType.CAR_SX || type == ObstacleType.TRAIN_DX
-				|| type == ObstacleType.TRAIN_SX);
+				|| type == ObstacleType.TRAIN_SX || type == ObstacleType.TRUNK_SX || type == ObstacleType.TRUNK_DX);
 		this.type = type;
 		this.moveLeft = (type == ObstacleType.CAR_SX || type== ObstacleType.TRAIN_SX || type == ObstacleType.TRUNK_SX) ? 1 : -1;
 		this.speed = this.moveLeft * 1;
