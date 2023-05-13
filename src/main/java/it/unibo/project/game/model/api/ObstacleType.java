b5e4b72215ae334a728d2bd8b11ded62583880f9
Daniele Muffato <daniele.muffato@studio.unibo.it>
2023-05-13 12:55:03 +0200

    [FEAT] added obstacle type isWalkableOn parameter

--- src/main/java/it/unibo/project/game/model/api/ObstacleType.java
+++ src/main/java/it/unibo/project/game/model/api/ObstacleType.java
@@ -3,36 +3,39 @@ package it.unibo.project.game.model.api;
 import it.unibo.project.controller.core.impl.LauncherImpl;
 
 public enum ObstacleType {
-    TREE(0, 0, 0, 0),
-    BUSH(0, 0, 0, 0),
-    FENCE(0, 0, 0, 0),
-    STOPLIGHT(0, 0, 0, 0),
-    TRANSPARENT_OBSTACLE(0, 0, 0, 0),
-    CAR_DX(5, 1, 8, LauncherImpl.ORIZ_CELL + 1),
-    CAR_SX(-5, -1, -8, LauncherImpl.ORIZ_CELL + 1),
-    TRAIN_DX(30, 20, 50, LauncherImpl.ORIZ_CELL * 4),
-    TRAIN_SX(-30, -20, -50, LauncherImpl.ORIZ_CELL * 4),
-    WAGON_DX(30, 20, 50, LauncherImpl.ORIZ_CELL * 4),
-    WAGON_SX(-30, -20, -50, LauncherImpl.ORIZ_CELL * 4),
-    TRUNK_START_DX(2, 2, 6, LauncherImpl.ORIZ_CELL + 1),
-    TRUNK_DX(2, 2, 6, LauncherImpl.ORIZ_CELL + 1),
-    TRUNK_FINISH_DX(2, 2, 6, LauncherImpl.ORIZ_CELL + 1),
-    TRUNK_START_SX(-2, -2, -6, LauncherImpl.ORIZ_CELL + 1),
-    TRUNK_SX(-2, -2, -6, LauncherImpl.ORIZ_CELL + 1),
-    TRUNK_FINISH_SX(-2, -2, -6, LauncherImpl.ORIZ_CELL + 1),
-    BIKE_DX(3, 1, 4, LauncherImpl.ORIZ_CELL * 4),
-    BIKE_SX(-3, -1, -4, LauncherImpl.ORIZ_CELL + 1);
+    TREE(0, 0, 0, 0, false),
+    BUSH(0, 0, 0, 0, false),
+    FENCE(0, 0, 0, 0, false),
+    STOPLIGHT(0, 0, 0, 0, false),
+    TRANSPARENT_OBSTACLE(0, 0, 0, 0, false),
+    CAR_DX(5, 1, 8, LauncherImpl.ORIZ_CELL + 1, false),
+    CAR_SX(-5, -1, -8, LauncherImpl.ORIZ_CELL + 1, false),
+    TRAIN_DX(30, 20, 50, LauncherImpl.ORIZ_CELL * 4, false),
+    TRAIN_SX(-30, -20, -50, LauncherImpl.ORIZ_CELL * 4, false),
+    WAGON_DX(30, 20, 50, LauncherImpl.ORIZ_CELL * 4, false),
+    WAGON_SX(-30, -20, -50, LauncherImpl.ORIZ_CELL * 4, false),
+    TRUNK_START_DX(2, 2, 6, LauncherImpl.ORIZ_CELL + 1, true),
+    TRUNK_DX(2, 2, 6, LauncherImpl.ORIZ_CELL + 1, true),
+    TRUNK_FINISH_DX(2, 2, 6, LauncherImpl.ORIZ_CELL + 1, true),
+    TRUNK_START_SX(-2, -2, -6, LauncherImpl.ORIZ_CELL + 1, true),
+    TRUNK_SX(-2, -2, -6, LauncherImpl.ORIZ_CELL + 1, true),
+    TRUNK_FINISH_SX(-2, -2, -6, LauncherImpl.ORIZ_CELL + 1, true),
+    BIKE_DX(3, 1, 4, LauncherImpl.ORIZ_CELL * 4, false),
+    BIKE_SX(-3, -1, -4, LauncherImpl.ORIZ_CELL + 1, false);
 
     private final int wrapAroundDim;
     private final double speed;
     private final double minSpeed;
     private final double maxSpeed;
+    private final boolean isWalkableOn;
 
-    private ObstacleType(final double speed, final double minSpeed, final double maxSpeed, final int wrapAroundDim) {
+    private ObstacleType(final double speed, final double minSpeed, final double maxSpeed,
+            final int wrapAroundDim, final boolean isWalkableOn) {
         this.speed = speed;
         this.wrapAroundDim = wrapAroundDim;
         this.minSpeed = minSpeed;
         this.maxSpeed = maxSpeed;
+        this.isWalkableOn = isWalkableOn;
     }
 
     public double getSpeed() {
@@ -53,5 +56,9 @@ public enum ObstacleType {
 
     public double getMaxSpeed() {
         return this.maxSpeed;
+    }
+
+    public boolean isWalkableOn() {
+        return this.isWalkableOn;
     }
 }
