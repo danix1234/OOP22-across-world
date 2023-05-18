1a5f45429b49d0b4617f99b6d8d1a172bfacc187
Daniele Muffato <daniele.muffato@studio.unibo.it>
2023-05-18 14:09:29 +0200

    [FEAT] added parameters for collectableType

--- src/main/java/it/unibo/project/game/model/api/CollectableType.java
+++ src/main/java/it/unibo/project/game/model/api/CollectableType.java
@@ -1,8 +1,32 @@
 package it.unibo.project.game.model.api;
 
+import java.util.Optional;
+
 public enum CollectableType {
-    COIN,
-    POWERUP_COIN_MULTIPLIER,
-    POWERUP_COIN_MAGNET,
-    POWERUP_IMMORTALITY,
+    COIN(true, false, Optional.of(1)),
+    POWERUP_COIN_MULTIPLIER(false, true, Optional.empty()),
+    POWERUP_COIN_MAGNET(false, true, Optional.empty()),
+    POWERUP_IMMORTALITY(false, true, Optional.empty());
+
+    private final boolean isCoin;
+    private final boolean isPowerUp;
+    private final Optional<Integer> coinValue;
+
+    private CollectableType(final boolean isCoin, final boolean isPowerUp, final Optional<Integer> coinValue) {
+        this.isCoin = isCoin;
+        this.isPowerUp = isPowerUp;
+        this.coinValue = coinValue;
+    }
+
+    public boolean isCoin() {
+        return this.isCoin;
+    }
+
+    public boolean isPowerUp() {
+        return this.isPowerUp;
+    }
+
+    public Optional<Integer> getCoinValue() {
+        return this.coinValue;
+    }
 }
