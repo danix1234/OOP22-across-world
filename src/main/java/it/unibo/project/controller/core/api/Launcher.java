d5347102fcb3b14311dbc547505f220dee1a7cb3
AleToro7 <toryst4rk@gmail.com>
2023-05-31 23:20:32 +0200

    Risolti warnings checkStyle di Launcher

--- src/main/java/it/unibo/project/controller/core/api/Launcher.java
+++ src/main/java/it/unibo/project/controller/core/api/Launcher.java
@@ -7,10 +7,8 @@ import it.unibo.project.game.logic.api.HandlePowerup;
 import it.unibo.project.game.model.api.BackgroundCell;
 import it.unibo.project.game.model.api.Collectable;
 import it.unibo.project.game.model.api.GameStat;
-import it.unibo.project.game.model.api.GameWorld;
 import it.unibo.project.game.model.api.Obstacle;
 import it.unibo.project.game.model.api.Player;
-import it.unibo.project.input.api.Action;
 import it.unibo.project.input.api.InputHandler;
 import it.unibo.project.utility.Vector2D;
 import it.unibo.project.view.api.Scene;
@@ -57,24 +55,29 @@ public interface Launcher {
 
     /**
      * converter position in cell to position in pixel.
+     * @param cellPos Vector2D that contains cell position (x,y)
+     * @return double that represent x value of cell in pixel pos
      */
     double convertCellToPixelPos(Vector2D cellPos);
 
-    /**
-     * converter position in pixel to position in cell.
-     */
+   /**
+    * converter position in pixel to position in cell.
+    * @param pixelX double that represent pixel x that msut be converted to cell x
+    * @param cellY int  cell y position
+    * @return Vector2D that contains the cell position (x,y)
+    */
     Vector2D convertPixelToCellPos(double pixelX, int cellY);
 
     /**
      * convert obstaclePixelX into the actual pixel on the screen.
      * 
-     * @param pixelX
+     * @param obstaclePixelX pixel x value of the obstacle
      * @return actual pixel x position
      */
     double getActualPixelX(double obstaclePixelX);
 
     /**
-     * inverse function of {@link #getActualPixelX(double)}
+     * inverse function of {@link #getActualPixelX(double)}.
      * 
      * @param actualPixelX
      * @return obstacle pixel x position
@@ -84,12 +87,12 @@ public interface Launcher {
     // MODEL methods
 
     /**
-     * @return {@linkplain Difficuly} for the current/next game
+     * @return {@linkplain Difficuly} for the current/next game.
      */
     Difficulty getDifficulty();
 
     /**
-     * @param difficulty difficulty for the next game
+     * @param difficulty difficulty for the next game.
      */
     void setDifficulty(Difficulty difficulty);
 
@@ -99,38 +102,38 @@ public interface Launcher {
     Player getPlayer();
 
     /**
-     * @return collection of all {@linkplain Obstacle} in the entire map
+     * @return collection of all {@linkplain Obstacle} in the entire map.
      */
     List<Obstacle> getObstacles();
 
     /**
-     * @return collection of all {@linkplain Collectable} in the entire map
+     * @return collection of all {@linkplain Collectable} in the entire map.
      */
     List<Collectable> getCollectables();
 
     /**
-     * @return collection of all {@linkplain BackgroundCell} in the entire map
+     * @return collection of all {@linkplain BackgroundCell} in the entire map.
      */
     List<BackgroundCell> getBackgroundCells();
 
     /**
-     * @return {@linkplain Loader} which manages save and load operation from file
+     * @return {@linkplain Loader} which manages save and load operation from file.
      */
     Loader getLoader();
 
     /**
-     * loads map from file in {@linkplain GameWorld}
+     * loads map from file in {@linkplain GameWorld}.
      */
     void loadMap();
 
     /**
-     * @return updated {@linkplain GameStat}
+     * @return updated {@linkplain GameStat}.
      */
     GameStat getGameStat();
 
     /**
      * suggests {@code MovementLogic} to move player to [x,y] if there are no
-     * obstacles in the way
+     * obstacles in the way.
      * 
      * @param x
      * @param y
@@ -140,7 +143,7 @@ public interface Launcher {
     void movePlayerIfPossible(int x, int y);
 
     /**
-     * force {@code MovementLogic} to move dynamic obstacles (cars, trains, trunks)
+     * force {@code MovementLogic} to move dynamic obstacles (cars, trains, trunks).
      */
     void moveDynamicObstacles();
 
@@ -159,7 +162,7 @@ public interface Launcher {
     void start();
 
     /**
-     * @param sceneType of the inputHandler needed
+     * @param sceneType of the inputHandler needed.
      * @return {@linkplain InputHandler} which handles the {@linkplain Action}
      */
     InputHandler getInputHandler(SceneType sceneType);
@@ -172,14 +175,14 @@ public interface Launcher {
     void startEngine();
 
     /**
-     * get the collision checker
+     * get the collision checker.
      * 
      * @return {@linkplain CheckCollision} to check any collision between entities
      */
     CheckCollision getCheckCollision();
 
     /**
-     * get the powerup handler
+     * get the powerup handler.
      * 
      * @return {@linkplain HandlePowerup} to manage when a powerup is collected
      */
