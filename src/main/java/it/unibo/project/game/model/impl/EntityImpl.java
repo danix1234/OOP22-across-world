5978e3f1ca1ba59208b6c766c0291fd4f4f25333
AleToro7 <toryst4rk@gmail.com>
2023-05-31 23:19:06 +0200

    Risolti warnings checkStyle di EntityImpl

--- src/main/java/it/unibo/project/game/model/impl/EntityImpl.java
+++ src/main/java/it/unibo/project/game/model/impl/EntityImpl.java
@@ -3,35 +3,42 @@ package it.unibo.project.game.model.impl;
 import it.unibo.project.game.model.api.Entity;
 import it.unibo.project.utility.Vector2D;
 
+/**
+ * Class {@code EntityImpl}, implements {@link Entity}.
+ */
 public class EntityImpl implements Entity {
+    private Vector2D entityPos;
+    private final boolean movable;
 
-	private Vector2D entityPos;
-	private final boolean movable;
+    /**
+     * Constructor of class Entity, set the initial postition and if movable of the
+     * Entity with the value of
+     * the given param.
+     * 
+     * @param initialPos Vector2D that contains the initial position to give to the
+     *                   entity
+     * @param movable    boolean that is set to true if the entity is movable
+     */
+    public EntityImpl(final Vector2D initialPos, final boolean movable) {
+        this.entityPos = initialPos;
+        this.movable = movable;
+    }
 
-	/**
-	 * Constructor of class Entity.
-	 */
-	public EntityImpl(final Vector2D initialPos, final boolean movable) {
-		this.entityPos = initialPos;
-		this.movable = movable;
-	}
+    @Override
+    public final Vector2D getPosition() {
+        return this.entityPos;
+    }
 
-	@Override
-	public Vector2D getPosition() {
-		return this.entityPos;
-	}
+    @Override
+    public final boolean isMovable() {
+        return this.movable;
+    }
 
-	@Override
-	public boolean isMovable() {
-		return this.movable;
-	}
-
-	@Override
-	public void move(final int x, final int y) {
-		if (movable) {
-			final Vector2D newPos = new Vector2D(x, y);
-			this.entityPos = newPos;
-		}
-	}
-
+    @Override
+    public void move(final int x, final int y) {
+        if (movable) {
+            final Vector2D newPos = new Vector2D(x, y);
+            this.entityPos = newPos;
+        }
+    }
 }
