5d980a9b68384b721daa472e032c9ea4af69926c
AleToro7 <toryst4rk@gmail.com>
2023-05-31 23:19:31 +0200

    Risolti warnings checkStyle di BackGroundCellImpl

--- src/main/java/it/unibo/project/game/model/impl/BackgroundCellImpl.java
+++ src/main/java/it/unibo/project/game/model/impl/BackgroundCellImpl.java
@@ -4,18 +4,28 @@ import it.unibo.project.game.model.api.BackgroundCell;
 import it.unibo.project.game.model.api.BackgroundCellType;
 import it.unibo.project.utility.Vector2D;
 
-public class BackgroundCellImpl extends EntityImpl implements BackgroundCell {
+/**
+ * Class {@code BackgroundCellImpl}, contains methods to manage background cells.
+ * extends {@link EntityImpl} and implements {@link BackgroundCell}
+ */
+public final class BackgroundCellImpl extends EntityImpl implements BackgroundCell {
+    private BackgroundCellType type;
 
-	BackgroundCellType type;
+    /**
+     * constructor of BackgroundCellImpl, set the initial postition and type of the background cell with the value of
+     * the given param.
+     * 
+     * @param initialPos Vector2D that contains the initial position to give to the background cell
+     * @param type BackgroundCellType that contains the type to attribute to the background cell
+     */
+    public BackgroundCellImpl(final Vector2D initialPos, final BackgroundCellType type) {
+        super(initialPos, false);
+        this.type = type;
+    }
 
-	public BackgroundCellImpl(final Vector2D initialPos, final BackgroundCellType type) {
-		super(initialPos, false);
-		this.type = type;
-	}
-
-	@Override
-	public BackgroundCellType getType() {
-		return this.type;
-	}
+    @Override
+    public BackgroundCellType getType() {
+        return this.type;
+    }
 
 }
