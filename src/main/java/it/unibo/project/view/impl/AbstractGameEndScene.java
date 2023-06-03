a48546c7da56b121d8cb27059025690a3f2f8bde
Gledis Ujkaj <gledis.ujkaj@studio.unibo.it>
2023-06-03 15:01:11 +0200

    fix some warnings

--- src/main/java/it/unibo/project/view/impl/AbstractGameEndScene.java
+++ src/main/java/it/unibo/project/view/impl/AbstractGameEndScene.java
@@ -6,12 +6,10 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-
 import it.unibo.project.controller.core.api.SceneType;
 import it.unibo.project.input.api.Action;
 
@@ -21,20 +19,30 @@ public abstract class AbstractGameEndScene extends AbstractScene {
     private JButton backToMenu;
     private JButton exitButton;
     private JLabel titleLabel;
+    private static final int BACKGROUND_RED = 40;
+    private static final int BACKGROUND_GREEN = 40;
+    private static final int BACKGROUND_BLUE = 40;
+    private static final int TITLE_LABEL_HEIGHT = 50;
+    private static final int WHITE_RED = 255;
+    private static final int WHITE_GREEN = 255;
+    private static final int WHITE_BLUE = 255;
+    private static final int BUTTON_WIDTH = 200;
+    private static final int BUTTON_HEIGHT = 70;
+    private static final int FONT_SIZE = 20;
 
-    protected AbstractGameEndScene(String title, Action tryAgainAction, Action backToMenuAction, Action exitButtonAction, SceneType type) {
-
+    protected AbstractGameEndScene(final String title, final Action tryAgainAction, 
+    final Action backToMenuAction, final Action exitButtonAction, final SceneType type) {
 
         // pannello principale
         this.panel = new JPanel(new GridBagLayout());
         this.panel.setLayout(new GridBagLayout());
-        this.panel.setBackground(new Color(40, 40, 40));
+        this.panel.setBackground(new Color(BACKGROUND_RED, BACKGROUND_GREEN, BACKGROUND_BLUE));
 
         // creazione del titolo del gioco
         titleLabel = new JLabel(title);
         titleLabel.setForeground(Color.WHITE);
         titleLabel.setFont(new Font("Arial", Font.BOLD, 100));
-        titleLabel.setBounds(0, 10, panel.getWidth(), 50);
+        titleLabel.setBounds(0, 10, panel.getWidth(), TITLE_LABEL_HEIGHT);
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
         // creazione dei pulsanti del menu
@@ -47,20 +55,20 @@ public abstract class AbstractGameEndScene extends AbstractScene {
         exitButton.setFocusPainted(false);
 
         // impostazioni dei buttons
-        tryAgainButton.setBackground(new Color(255, 255, 255));
+        tryAgainButton.setBackground(new Color(WHITE_RED, WHITE_GREEN, WHITE_BLUE));
         tryAgainButton.setForeground(Color.BLACK);
-        tryAgainButton.setFont(new Font("Arial", Font.BOLD, 20));
-        tryAgainButton.setPreferredSize(new Dimension(200, 70));
+        tryAgainButton.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+        tryAgainButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 
-        backToMenu.setBackground(new Color(255, 255, 255));
+        backToMenu.setBackground(new Color(WHITE_RED, WHITE_GREEN, WHITE_BLUE));
         backToMenu.setForeground(Color.BLACK);
-        backToMenu.setFont(new Font("Arial", Font.BOLD, 20));
-        backToMenu.setPreferredSize(new Dimension(200, 70));
+        backToMenu.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+        backToMenu.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 
-        exitButton.setBackground(new Color(255, 255, 255));
+        exitButton.setBackground(new Color(WHITE_RED, WHITE_GREEN, WHITE_BLUE));
         exitButton.setForeground(Color.RED);
-        exitButton.setFont(new Font("Arial", Font.BOLD, 20));
-        exitButton.setPreferredSize(new Dimension(200, 70));
+        exitButton.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+        exitButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 
         // aggiunta dei pulsanti al pannello principale
         GridBagConstraints gbc = new GridBagConstraints();
@@ -83,7 +91,6 @@ public abstract class AbstractGameEndScene extends AbstractScene {
         backToMenu.addActionListener(((e) -> getInputHandler(type).executeAction(backToMenuAction)));
         exitButton.addActionListener(((e) -> getInputHandler(type).executeAction(exitButtonAction)));
 
-
         setPanel(this.panel);
     }
 
@@ -95,16 +102,11 @@ public abstract class AbstractGameEndScene extends AbstractScene {
         return backToMenu;
     }
 
-
     protected JButton getExitButton() {
         return exitButton;
     }
 
     @Override
     public void update() {
-
     }
-
-    
-
 }
