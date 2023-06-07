a4c5386ec060428c6ff7b8c6e2bca684a506d579
Gledis Ujkaj <gledis.ujkaj@studio.unibo.it>
2023-06-07 16:53:22 +0200

    fixed some warnings

--- src/main/java/it/unibo/project/view/impl/AbstractGameEndScene.java
+++ src/main/java/it/unibo/project/view/impl/AbstractGameEndScene.java
@@ -14,11 +14,9 @@ import it.unibo.project.controller.core.api.SceneType;
 import it.unibo.project.input.api.Action;
 
 public abstract class AbstractGameEndScene extends AbstractScene {
-    private JPanel panel;
-    private JButton tryAgainButton;
-    private JButton backToMenu;
-    private JButton exitButton;
-    private JLabel titleLabel;
+    private final JButton tryAgainButton;
+    private final JButton backToMenu;
+    private final JButton exitButton;
     private static final int BACKGROUND_RED = 40;
     private static final int BACKGROUND_GREEN = 40;
     private static final int BACKGROUND_BLUE = 40;
@@ -29,19 +27,20 @@ public abstract class AbstractGameEndScene extends AbstractScene {
     private static final int BUTTON_WIDTH = 200;
     private static final int BUTTON_HEIGHT = 70;
     private static final int FONT_SIZE = 20;
+    private static final String FONT_NAME = "Arial";
 
     protected AbstractGameEndScene(final String title, final Action tryAgainAction, 
     final Action backToMenuAction, final Action exitButtonAction, final SceneType type) {
 
-        // pannello prin
-        this.panel = new JPanel(new GridBagLayout());
-        this.panel.setLayout(new GridBagLayout());
-        this.panel.setBackground(new Color(BACKGROUND_RED, BACKGROUND_GREEN, BACKGROUND_BLUE));
+        // pannello principale
+        final JPanel panel = new JPanel(new GridBagLayout());
+        panel.setLayout(new GridBagLayout());
+        panel.setBackground(new Color(BACKGROUND_RED, BACKGROUND_GREEN, BACKGROUND_BLUE));
 
         // creazione del titolo del gioco
-        titleLabel = new JLabel(title);
+        final JLabel titleLabel = new JLabel(title);
         titleLabel.setForeground(Color.WHITE);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 100));
+        titleLabel.setFont(new Font(FONT_NAME, Font.BOLD, 100));
         titleLabel.setBounds(0, 10, panel.getWidth(), TITLE_LABEL_HEIGHT);
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
@@ -57,41 +56,41 @@ public abstract class AbstractGameEndScene extends AbstractScene {
         // impostazioni dei buttons
         tryAgainButton.setBackground(new Color(WHITE_RED, WHITE_GREEN, WHITE_BLUE));
         tryAgainButton.setForeground(Color.BLACK);
-        tryAgainButton.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+        tryAgainButton.setFont(new Font(FONT_NAME, Font.BOLD, FONT_SIZE));
         tryAgainButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 
         backToMenu.setBackground(new Color(WHITE_RED, WHITE_GREEN, WHITE_BLUE));
         backToMenu.setForeground(Color.BLACK);
-        backToMenu.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+        backToMenu.setFont(new Font(FONT_NAME, Font.BOLD, FONT_SIZE));
         backToMenu.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 
         exitButton.setBackground(new Color(WHITE_RED, WHITE_GREEN, WHITE_BLUE));
         exitButton.setForeground(Color.RED);
-        exitButton.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+        exitButton.setFont(new Font(FONT_NAME, Font.BOLD, FONT_SIZE));
         exitButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 
         // aggiunta dei pulsanti al pannello principale
-        GridBagConstraints gbc = new GridBagConstraints();
+        final GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 0, 10, 0);
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.anchor = GridBagConstraints.CENTER;
-        this.panel.add(titleLabel);
+        panel.add(titleLabel);
 
         gbc.gridy = 1;
-        this.panel.add(tryAgainButton, gbc);
+        panel.add(tryAgainButton, gbc);
 
         gbc.gridy = 2;
-        this.panel.add(backToMenu, gbc);
+        panel.add(backToMenu, gbc);
 
         gbc.gridy = 3;
-        this.panel.add(exitButton, gbc);
+        panel.add(exitButton, gbc);
 
-        tryAgainButton.addActionListener(((e) -> getInputHandler(type).executeAction(tryAgainAction)));
-        backToMenu.addActionListener(((e) -> getInputHandler(type).executeAction(backToMenuAction)));
-        exitButton.addActionListener(((e) -> getInputHandler(type).executeAction(exitButtonAction)));
+        tryAgainButton.addActionListener((e) -> getInputHandler(type).executeAction(tryAgainAction));
+        backToMenu.addActionListener((e) -> getInputHandler(type).executeAction(backToMenuAction));
+        exitButton.addActionListener((e) -> getInputHandler(type).executeAction(exitButtonAction));
 
-        setPanel(this.panel);
+        setPanel(panel);
     }
 
     protected JButton getTryAgainButton() {
--- src/main/java/it/unibo/project/view/impl/MenuScene.java
+++ src/main/java/it/unibo/project/view/impl/MenuScene.java
@@ -22,13 +22,12 @@ import it.unibo.project.controller.core.impl.LauncherImpl;
 import it.unibo.project.input.api.Action;
 
 public class MenuScene extends AbstractScene {
-    private JPanel panel;
-    private JButton startButton;
-    private JComboBox<String> difficultyComboBox;
-    private JButton exitButton;
-    private JButton shopButton;
-    private JButton clearProgressButton;
-    private JLabel titleLabel;
+    //private JPanel panel;
+    //private final JButton startButton;
+    private final JComboBox<String> difficultyComboBox;
+    //private final JButton exitButton;
+    //private final JButton shopButton;
+    //private final JButton clearProgressButton;
     private static final int PANEL_BACKGROUND_RED = 40;
     private static final int PANEL_BACKGROUND_GREEN = 40;
     private static final int PANEL_BACKGROUND_BLUE = 40;
@@ -41,30 +40,31 @@ public class MenuScene extends AbstractScene {
     private static final int BUTTON_HEIGHT = 70;
     private static final int FONT_SIZE = 20;
     private static final int GRID_Y_VALUE = 5;
+    private static final String FONT_NAME = "Arial";
 
     public MenuScene() {
         // pannello principale
-        this.panel = new JPanel();
-        this.panel.setLayout(new GridBagLayout());
-        this.panel.setBackground(new Color(PANEL_BACKGROUND_RED, PANEL_BACKGROUND_GREEN, PANEL_BACKGROUND_BLUE));
+        final JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        panel.setBackground(new Color(PANEL_BACKGROUND_RED, PANEL_BACKGROUND_GREEN, PANEL_BACKGROUND_BLUE));
 
         // creazione del titolo del gioco
-        titleLabel = new JLabel("Across the world");
+        final JLabel titleLabel = new JLabel("Across the world");
         titleLabel.setForeground(Color.WHITE);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, TITLE_FONT_SIZE));
+        titleLabel.setFont(new Font(FONT_NAME, Font.BOLD, TITLE_FONT_SIZE));
         titleLabel.setBounds(0, 10, panel.getWidth(), TITLE_HEIGHT);
         titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
         // creazione dei buttons del menu
-        startButton = new JButton("START");
-        var difficulties = Arrays
+        final JButton startButton = new JButton("START");
+        final var difficulties = Arrays
                 .stream(Difficulty.values())
                 .map(Enum::name)
                 .toArray(String[]::new);
         difficultyComboBox = new JComboBox<>(difficulties);
-        shopButton = new JButton("SHOP");
-        clearProgressButton = new JButton("CLEAR PROGRESS");
-        exitButton = new JButton("SAVE AND EXIT");
+        final JButton shopButton = new JButton("SHOP");
+        final JButton clearProgressButton = new JButton("CLEAR PROGRESS");
+        final JButton exitButton = new JButton("SAVE AND EXIT");
         // bug fix: difficolty didn't change to what is shown
         LauncherImpl.LAUNCHER.setDifficulty(Difficulty.valueOf(difficultyComboBox.getSelectedItem() + ""));
 
@@ -78,79 +78,84 @@ public class MenuScene extends AbstractScene {
         // estetica dei buttons
         startButton.setBackground(new Color(WHITE_RED, WHITE_GREEN, WHITE_BLUE));
         startButton.setForeground(Color.BLACK);
-        startButton.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+        startButton.setFont(new Font(FONT_NAME, Font.BOLD, FONT_SIZE));
         startButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 
         difficultyComboBox.setBackground(new Color(WHITE_RED, WHITE_GREEN, WHITE_BLUE));
         difficultyComboBox.setForeground(Color.BLACK);
-        difficultyComboBox.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+        difficultyComboBox.setFont(new Font(FONT_NAME, Font.BOLD, FONT_SIZE));
         difficultyComboBox.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 
         shopButton.setBackground(new Color(WHITE_RED, WHITE_GREEN, WHITE_BLUE));
         shopButton.setForeground(Color.BLACK);
-        shopButton.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+        shopButton.setFont(new Font(FONT_NAME, Font.BOLD, FONT_SIZE));
         shopButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 
         clearProgressButton.setBackground(new Color(WHITE_RED, WHITE_GREEN, WHITE_BLUE));
         clearProgressButton.setForeground(Color.BLACK);
-        clearProgressButton.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+        clearProgressButton.setFont(new Font(FONT_NAME, Font.BOLD, FONT_SIZE));
         clearProgressButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 
         exitButton.setBackground(new Color(WHITE_RED, WHITE_GREEN, WHITE_BLUE));
         exitButton.setForeground(Color.RED);
-        exitButton.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+        exitButton.setFont(new Font(FONT_NAME, Font.BOLD, FONT_SIZE));
         exitButton.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 
         // aggiunta i pulsanti al pannello principale
-        GridBagConstraints gbc = new GridBagConstraints();
+        final GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.insets = new Insets(10, 0, 10, 0);
         gbc.anchor = GridBagConstraints.CENTER;
-        this.panel.add(titleLabel);
+        panel.add(titleLabel);
         gbc.gridy = 1;
-        this.panel.add(startButton, gbc);
+        panel.add(startButton, gbc);
         gbc.gridy = 2;
-        this.panel.add(difficultyComboBox, gbc);
+        panel.add(difficultyComboBox, gbc);
         gbc.gridy = 3;
-        this.panel.add(shopButton, gbc);
+        panel.add(shopButton, gbc);
         gbc.gridy = 4;
-        this.panel.add(clearProgressButton, gbc);
+        panel.add(clearProgressButton, gbc);
         gbc.gridy = GRID_Y_VALUE;
-        this.panel.add(exitButton, gbc);
+        panel.add(exitButton, gbc);
 
         // aggiungo gli ActionListener
         startButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(final ActionEvent e) {
                 getInputHandler(SceneType.MENU).executeAction(Action.CHANGE_SCENE_TO_GAME);
             }
         });
 
         difficultyComboBox.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(final ActionEvent e) {
-                String selectedDifficulty = (String) difficultyComboBox.getSelectedItem();
+                final String selectedDifficulty = (String) difficultyComboBox.getSelectedItem();
                 LauncherImpl.LAUNCHER.setDifficulty(Difficulty.valueOf(selectedDifficulty));
             }
         });
 
         shopButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(final ActionEvent e) {
                 getInputHandler(SceneType.MENU).executeAction(Action.CHANGE_SCENE_TO_SHOP);
             }
         });
 
         clearProgressButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(final ActionEvent e) {
                 //LauncherImpl.LAUNCHER.deleteStatFile();
             }
         });
 
         exitButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(final ActionEvent e) {
                 getInputHandler(SceneType.MENU).executeAction(Action.EXIT_APP);
             }
         });
-        setPanel(this.panel);
+        setPanel(panel);
     }
 
     @Override
